a570e8f9fa0e2ea152a2f47bae8b2e2cadcf7d4f
Victornfb <victornfb@outlook.com>
2022-06-15 20:30:12 -0300

    Refactor: Routes file

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,10 @@
 import express from 'express';
-import { categoriesRoutes } from './routes/categories.routes';
-import { specificationsRoutes } from './routes/specifications.routes';
+import { router } from './routes';
 
 const app = express();
-const port = 3333;
 
 app.use(express.json());
+app.use(router);
 
-app.use('/categories', categoriesRoutes);
-
-app.use('/specifications', specificationsRoutes);
-
+const port = 3333;
 app.listen(port, () => console.log('Server is now running on port ' + port));
