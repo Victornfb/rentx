d965f7999f0ff8c4d526ca30f1d11f7c57bca525
Victornfb <victornfb@outlook.com>
2022-06-16 12:27:50 -0300

    Remove file when finish import

--- src/modules/cars/useCases/importCategories/ImportCategoriesUseCase.ts
+++ src/modules/cars/useCases/importCategories/ImportCategoriesUseCase.ts
@@ -24,6 +24,7 @@ class ImportCategoriesUseCase {
 				categories.push({ name, description });
 			})
 			.on('end', () => {
+				fs.promises.unlink(file.path);
 				resolve(categories);
 			})
 			.on('error', (err) => {
