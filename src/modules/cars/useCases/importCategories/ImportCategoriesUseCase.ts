88b1826bde2db64d07344242e0858537f562133f
Victornfb <victornfb@outlook.com>
2022-06-15 21:28:01 -0300

    Feat: Read file stream and create categories

--- src/modules/cars/useCases/importCategories/ImportCategoriesUseCase.ts
+++ src/modules/cars/useCases/importCategories/ImportCategoriesUseCase.ts
@@ -1,7 +1,53 @@
+import fs from 'fs';
+import { parse } from 'csv-parse';
+import { CategoriesRepository } from '../../repositories/implementations/CategoriesRepository';
+
+interface IImportCategory {
+	name: string;
+	description: string;
+}
+
 class ImportCategoriesUseCase {
-	execute(file: any) {
-		console.log(file);
+	constructor(private categoriesRepository: CategoriesRepository) {}
+
+	loadCategories(file: Express.Multer.File): Promise<IImportCategory[]> {
+		return new Promise((resolve, reject) => {
+			const stream = fs.createReadStream(file.path);
+			const categories: IImportCategory[] = [];
+
+			const parseFile = parse();
+
+			stream.pipe(parseFile);
+
+			parseFile.on('data', async (line) => {
+				const [name, description] = line;
+				categories.push({ name, description });
+			})
+			.on('end', () => {
+				resolve(categories);
+			})
+			.on('error', (err) => {
+				reject(err);
+			});
+		});
+	}
+
+	async execute(file: Express.Multer.File): Promise<void> {
+		const categories = await this.loadCategories(file);
+		
+		categories.map(async (category) => {
+			const { name, description } = category;
+
+			const categoryAlreadyExists = this.categoriesRepository.findByName(name);
+
+			if (!categoryAlreadyExists) {
+				this.categoriesRepository.create({
+					name,
+					description,
+				})
+			}
+		})
 	}
 }
 
-export { ImportCategoriesUseCase }
+export { ImportCategoriesUseCase };
