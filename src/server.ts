e7888b9a5e1fad0892b25c552e90ab32fa893ed9
Victornfb <victornfb@outlook.com>
2022-06-16 12:24:47 -0300

    Add swagger documentation

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,14 @@
 import express from 'express';
+import SwaggerUi from 'swagger-ui-express';
 import { router } from './routes';
+import swaggerFile from './swagger.json';
 
 const app = express();
 
 app.use(express.json());
+
+app.use('/api-docs', SwaggerUi.serve, SwaggerUi.setup(swaggerFile));
+
 app.use(router);
 
 const port = 3333;
