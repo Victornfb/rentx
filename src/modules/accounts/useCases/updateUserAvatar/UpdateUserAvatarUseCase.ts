5800cc8d7f13728a9118bd9a538b25da273bab5d
Victornfb <victornfb@outlook.com>
2022-07-20 19:09:05 -0300

    Fix: Spell 'Request'

--- src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
+++ src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
@@ -2,7 +2,7 @@ import { IUsersRepository } from "@modules/accounts/repositories/IUserRepository
 import { deleteFile } from "@utils/file";
 import { inject, injectable } from "tsyringe";
 
-interface IRquest {
+interface IRequest {
 	user_id: string;
 	avatar_file: string;
 }
@@ -14,7 +14,7 @@ class UpdateUserAvatarUseCase {
 		private usersRepository: IUsersRepository
 	) {}
 
-	async execute({ user_id, avatar_file}: IRquest): Promise<void> {
+	async execute({ user_id, avatar_file}: IRequest): Promise<void> {
 		const user = await this.usersRepository.findById(user_id);
 
 		if (user.avatar) {
