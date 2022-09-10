5671e58901167ce070ddd10893349b803ccb33ac
Victornfb <victornfb@outlook.com>
2022-09-10 12:26:27 -0300

    Refactor: Change from fs system to storage provider

--- src/modules/cars/useCases/uploadCarImages/UploadCarImagesUseCase.ts
+++ src/modules/cars/useCases/uploadCarImages/UploadCarImagesUseCase.ts
@@ -1,4 +1,5 @@
-import { CarsImagesRepository } from "@modules/cars/infra/typeorm/repositories/CarsImagesRepository";
+import { ICarsImagesRepository } from "@modules/cars/repositories/ICarsImagesRepository";
+import { IStorageProvider } from "@shared/container/providers/StorageProvider/IStorageProvider";
 import { deleteFile } from "@utils/file";
 import { inject, injectable } from "tsyringe";
 
@@ -11,20 +12,23 @@ interface IRequest {
 class UploadCarImagesUseCase {
 	constructor(
 		@inject('CarsImagesRepository')
-		private carsImagesRepository: CarsImagesRepository
+		private carsImagesRepository: ICarsImagesRepository,
+		@inject('StorageProvider')
+		private storageProvider: IStorageProvider
 	) {}
 
 	async execute({ car_id, images_name}: IRequest): Promise<void> {
 		const carImages = await this.carsImagesRepository.findByCarId(car_id);
 
 		carImages.map(async (image) => {
-			await deleteFile(`./tmp/cars/${image.image_name}`);
+			await this.storageProvider.delete(image.image_name, 'cars');
 		});
 
 		await this.carsImagesRepository.deleteByCarId(car_id);
 
 		images_name.map(async (image) => {
 			await this.carsImagesRepository.create(car_id, image);
+			await this.storageProvider.save(image, 'cars');
 		});
 	}
 }
