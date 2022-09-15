0bd7a88c42c0c55bb31781a2a68ca610acd3b919
Victornfb <victornfb@outlook.com>
2022-09-15 17:35:20 -0300

    Feat: Add sentry error notifications

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -4,6 +4,8 @@ import cors from 'cors';
 import express, { NextFunction, Request, Response } from 'express';
 import 'express-async-errors';
 import SwaggerUi from 'swagger-ui-express';
+import * as Sentry from "@sentry/node";
+import * as Tracing from "@sentry/tracing";
 import { router } from '@shared/infra/http/routes';
 import swaggerFile from '../../../swagger.json';
 import createConnection from '@shared/infra/typeorm';
@@ -19,12 +21,28 @@ app.use(express.json());
 
 app.use(rateLimiter);
 
+if (process.env.NODE_ENV === 'production') {
+	Sentry.init({
+		dsn: process.env.SENTRY_DSN,
+		integrations: [
+			new Sentry.Integrations.Http({ tracing: true }),
+			new Tracing.Integrations.Express({ app }),
+		],
+		tracesSampleRate: 1.0,
+	});
+
+	app.use(Sentry.Handlers.requestHandler());
+	app.use(Sentry.Handlers.tracingHandler());
+}
+
 app.use('/api-docs', SwaggerUi.serve, SwaggerUi.setup(swaggerFile));
 app.use('/avatar', express.static(`${upload.tmpFolder}/avatar`));
 app.use('/cars', express.static(`${upload.tmpFolder}/cars`));
 
 app.use(cors());
 app.use(router);
+
+app.use(Sentry.Handlers.errorHandler());
 
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
 	if (err instanceof AppError) {
